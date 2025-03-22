afb9a960a3789fbfb219cdaf5bf3d2a81699de40
Danylo-Rudenko <starb8408@gmail.com>
2025-03-22 14:34:23 +0100

    #10 Przygotowywanie kodu na wysłanie — przejmowanie zapytań do uczelnianego servera

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, jsonify
+from parser import parse_all_courses
+
+template_dir = os.path.abspath('../frontend/templates/')
+static_dir = os.path.abspath('../frontend/static/')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/parse')
+def parse_data():
+    return jsonify(parse_all_courses())
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
